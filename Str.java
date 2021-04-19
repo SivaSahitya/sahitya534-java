27084f201de9f528c6794fc4b785aa694eda4644
SivaSahitya <82566266+SivaSahitya@users.noreply.github.com>
2021-04-19 10:04:19 +0530

    Create Str.java

---
+++ Str.java
@@ -0,0 +1,16 @@
+import java.lang.StringBuffer;
+import java.lang.String;
+class Str
+{
+ public static void main(String args[])
+ {
+ StringBuffer s1=new StringBuffer("ADITYA");
+ StringBuffer s2=new StringBuffer("COLLEGE");
+ System.out.println("before deletion s1:"+s1);
+ System.out.println("After deletion:");
+ System.out.println(s1.deleteCharAt(3));
+ System.out.println("before deletion s2:"+s2);
+ System.out.println("after deletion:");
+ System.out.println(s2.deleteCharAt(4));
+}
+}
