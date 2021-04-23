7094ceb4264238e4f650feaf323e3cd082cd05bc
SivaSahitya <82566266+SivaSahitya@users.noreply.github.com>
2021-04-23 18:23:25 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,20 @@
+package sample;
+public class Showp
+{
+public void pac()
+{
+System.out.println("hello world");
+}
+}
+
+---------------------------
+
+import sample.Showp;
+class Demo 
+{
+public static void main(String args[])
+{
+Showp s=new Showp();
+s.pac();
+}
+}
